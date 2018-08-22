f0b9133803b378c301555311cf06c1644bfcc5f4
LahiruTjay <lahiru.tjay@gmail.com>
2018-08-22 23:11:33 +0530

    Added DBb support

--- src/main/java/com/example/jwt/auth/JWTProvider.java
+++ src/main/java/com/example/jwt/auth/JWTProvider.java
@@ -2,4 +2,10 @@ package com.example.jwt.auth;
 
 public class JWTProvider {
 
+    public String createToken() {
+        
+        // Get claims to add user details and authorities
+        return "";
+    }
+    
 }
