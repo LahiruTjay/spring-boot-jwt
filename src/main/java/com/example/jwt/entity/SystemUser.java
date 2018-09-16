78eb2c99b0adf748846132ae32f2109164a80577
LahiruTjay <lahiru.tjay@gmail.com>
2018-09-16 20:58:04 +0530

    DAO n Entities updated

--- src/main/java/com/example/jwt/entity/SystemUser.java
+++ src/main/java/com/example/jwt/entity/SystemUser.java
@@ -1,10 +1,28 @@
 package com.example.jwt.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class SystemUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String username;
+    
     private String password;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
+    
     public SystemUser() {
         super();
     }
