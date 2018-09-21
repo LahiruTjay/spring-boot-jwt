647cd45a9b8a151c6a9455f2e35fbb733f558b45
LahiruTjay <lahiru.tjay@gmail.com>
2018-09-21 22:59:43 +0530

    DAO testing

--- src/main/java/com/example/jwt/service/CustomUserDetailsService.java
+++ src/main/java/com/example/jwt/service/CustomUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.example.jwt.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,13 +9,20 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.jwt.entity.SystemUser;
+import com.example.jwt.repository.SystemUserDAO;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    SystemUserDAO systemUserDAO;
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         SystemUser systemUser = loadSystemUserByUsername(username);
+        SystemUser systemUser2 =  systemUserDAO.findByUsername(username);
+        System.out.println(systemUser2.toString());
+        
         return new User(systemUser.getUsername(), systemUser.getPassword(), AuthorityUtils.createAuthorityList("ROLE_USER")); // todo - get the user from the DB
     }
 
